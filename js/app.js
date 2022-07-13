e277fa7bc4015f73680e6b7e5f9a9464dd8eee34
Antoine Ameloot <ant.ameloot@gmail.com>
2022-07-13 11:50:01 +0200

    Mobile version

--- js/app.js
+++ js/app.js
@@ -3,3 +3,18 @@ window.addEventListener('scroll', function() {
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
 })
+
+function myFunction() {
+    var networks = document.getElementById("networks");
+    var menu = document.getElementById("header-menu");
+    var toggle = document.getElementById("menu-toggle-icon");
+    if (networks.style.display == "none") {
+      networks.style.display = "initial";
+      menu.style.display = "initial";
+      toggle.setAttribute("data-icon", "akar-icons:cross");
+    } else {
+      networks.style.display = "none";
+      menu.style.display = "none";
+      toggle.setAttribute("data-icon", "dashicons:menu-alt3");
+    }
+  }
